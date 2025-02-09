ead8a99e8d28f344b51cfa1258f36430f717aa9e
LucasDavis233513 <lucasdavis@unr.edu>
2025-02-08 19:26:12 -0800

    Added Spell Correction to the model

--- Assets/Scripts/SpeechRecognition/Src/API/Recognition.py
+++ Assets/Scripts/SpeechRecognition/Src/API/Recognition.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from tensorflow.keras.models import load_model
 
+from Data.NLP import NLP
 from Data.Process import Process
 from Model.ASRModel import ASRModel
 from ModelInterface import Interface
@@ -18,6 +19,7 @@ class SpeechRec(Interface):
 
         self.model = load_model(filePath, custom_objects = {'ctcloss': ASRModel.ctcloss}, safe_mode = False)
         self.process = Process()
+        self.NLP = NLP()
 
     def Predict(self, _audio):
         """
@@ -34,5 +36,22 @@ class SpeechRec(Interface):
         sentiment = self.model.predict(spectrogram)
         softmax = ASRModel.ctcDecoder(sentiment)
         prediction = self.process.ConvertLabel(softmax)
+
+        print(prediction)
+
+        return prediction
+
+    def PostProcess(self, _prediction):
+        """
+        Preform post processing on the models prediction to clean up the models
+        output.
+
+        Parameters:
+            - _prediction: A string of the prediction generated from the model
+
+        returns:
+            The cleaned up prediction
+        """
+        prediction = self.NLP.SpellingCorrection(_prediction)
 
         return prediction
