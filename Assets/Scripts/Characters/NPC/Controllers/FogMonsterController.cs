03dadfbf9bc3c99c706b3bfd4c8c42d027712d27
LucasDavis233513 <lucasdavis@unr.edu>
2025-04-24 20:32:20 -0700

    Init fog monster
    Got some animations done, need to implement the animator. Controller needs work.

---
+++ Assets/Scripts/Characters/NPC/Controllers/FogMonsterController.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using Characters.Player;
+using UnityEngine;
+using UnityEngine.AI;
+using Audio.SFX;
+using Characters.NPC;
+using Characters.NPC.Behavior_Tree;
+using Characters.NPC.Behavior_Tree.Strategies;
+using Characters.NPC.Behavior_Tree.Strategies.Conditional;
+using Characters.NPC.BlackboardSystem;
+using Characters.NPC.BlackboardSystem.Control;
+
+namespace Characters.NPC.Controllers {
+    public class FogMonsterController : MonoBehaviour {
+        [Header("Animator")]
+        private Animator animator;
+        
+        [Header("Audio")]
+        [SerializeField] AudioClip howlingSfx;
+        [SerializeField] AudioClip walkSfx;
+        [SerializeField] AudioClip alertSfx;
+        [SerializeField] AudioClip angrySfx;
+        [SerializeField] AudioClip attackSfx;
+        private SFXManager sfxManager {
+            get => SFXManager.instance;
+        }
+        
+        [Header("Combat")]
+        [SerializeField] private float stoppingDistance = 2.0f;
+        [SerializeField] float attackRange;
+        [SerializeField] float attackSpeed;
+        [SerializeField] float attackInterval;
+        public float hurtDistance = 0.25f;
+        private float timeOfLastAttack = 0;
+        private bool hasStopped = false;
+        private EnemyStats stats = null;
+        private Transform target;
+        
+        [Header("Emotes")]
+        private GameObject alertEmote;
+        private GameObject angryEmote;
+        private GameObject frustratedEmote;
+        
+        [Header("Movement")]
+        [SerializeField] float speed = 2f;
+        [SerializeField] float patrolRadius = 5f;
+        [SerializeField] float minIvenstigateDistance; 
+        [SerializeField] float maxIvenstigateDistance;
+        [SerializeField] private Transform patrolArea; 
+        private NavMeshAgent agent;
+        
+        [Header("Senses")]
+        [SerializeField] public float sightRange;       // How far can this enemy see
+        [SerializeField] public float hearingRange;     // How far can this enemy hear
+        [SerializeField] public float searchRadius;
+        
+        BehaviorTree tree;
+        public BlackboardController controller {
+            get => BlackboardController.instance;
+        }
+        Blackboard blackboard;
+        BlackboardKey soundKey, positionKey;
+
+        void Awake() {
+            // Navmesh agent
+            agent = GetComponent<NavMeshAgent>();
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+            
+            if (target == null) {
+                target = GameObject.Find("Player").GetComponent<Transform>();
+            }
+            
+            alertEmote = GameObject.Find("Emotes/Alert");
+            angryEmote = GameObject.Find("Emotes/Angry");
+            frustratedEmote = GameObject.Find("Emotes/Frustrated");
+            
+            alertEmote.SetActive(false);
+            angryEmote.SetActive(false);
+            frustratedEmote.SetActive(false);
+            
+            blackboard = controller.GetBlackboard();
+            soundKey = blackboard.GetOrRegisterKey("SoundMade");
+            positionKey = blackboard.GetOrRegisterKey("SoundsPosition");
+            
+            GetReferences();
+
+            DefineBehavior();
+        }
+        
+        void Start() {
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        void Update() {
+            UpdateAnimation();
+            tree.Process();
+        }
+
+        void DefineBehavior() {
+            tree = new BehaviorTree("Fog Monster");
+
+            PrioritySelector actions = new PrioritySelector("Logic");
+            
+            Sequence seenPlayer = new Sequence("Seen Player", 100);
+            seenPlayer.AddChild(new Leaf("is Player Invisible?", new Condition(() => !target.GetComponent<PlayerStats>().isInvisible)));
+            seenPlayer.AddChild(new Leaf("is Player In Range?", new Condition(() => Conditions.InRange(transform, sightRange))));
+            seenPlayer.AddChild(new Leaf("SFX", new ActionStrategy(() => sfxManager.PlaySFX(angrySfx, transform, 1f))));
+            // seenPlayer.AddChild(new Leaf("Emote", new ActionStrategy(() => StartCoroutine(ShowEmote(frustratedEmote)))));
+            seenPlayer.AddChild(new Leaf("Move to Player", new MoveToTarget(transform, agent, target, speed, 2f)));
+            actions.AddChild(seenPlayer);
+            
+            Sequence heardNoise = new Sequence("Investigate Noise", 50);
+            heardNoise.AddChild(new Leaf("is there an Active sound report?", new Condition( () => blackboard.TryGetValue(soundKey, out bool value) && value )));
+            heardNoise.AddChild(new Leaf("is Sound in Range?", new Condition(() => Conditions.InRange(transform, hearingRange))));
+            // heardNoise.AddChild(new Leaf("SFX", new ActionStrategy(() => sfxManager.PlaySFX(alertSfx, transform, 1f))));
+            heardNoise.AddChild(new Leaf("Emote", new ActionStrategy(() => StartCoroutine(ShowEmote(alertEmote)))));
+            heardNoise.AddChild(new Leaf("Delay before investigate", new WaitSeconds(1f)));
+            heardNoise.AddChild(new Leaf("Move to sound", new MoveToTarget(transform, agent, () => blackboard.TryGetValue(positionKey, out Vector2 value) ? value : Vector2.zero, speed, 0.25f)));
+            heardNoise.AddChild(new Leaf("Investigate Area", new Investigate(agent, () => blackboard.TryGetValue(positionKey, out Vector2 value) ? value : Vector2.zero, searchRadius, speed, 2)));
+            actions.AddChild(heardNoise);
+            
+            Sequence idle = new Sequence("Idle");
+            idle.AddChild(new Leaf("Delay", new WaitSeconds(1f)));
+            actions.AddChild(idle);
+            
+            tree.AddChild(actions);
+        }
+        
+        void UpdateAnimation() {
+            Vector2 velocity = agent.velocity;
+            float magnitude = velocity.magnitude;
+            
+            Vector2 dir = magnitude > 0.1f ? velocity.normalized : Vector2.zero;
+            
+            animator.SetFloat("MoveX", dir.x);
+            animator.SetFloat("MoveY", dir.y);
+            animator.SetFloat("MoveMagnitude", magnitude);
+
+            if (magnitude < 0.1f && dir == Vector2.zero) {
+                animator.SetFloat("LastMoveX", dir.x);
+                animator.SetFloat("LastMoveY", dir.y);
+            }
+        }
+        
+        /// <summary>
+        /// Briefly display a given emote
+        /// </summary>
+        IEnumerator ShowEmote(GameObject emote) {
+            emote.SetActive(true);
+            yield return new WaitForSeconds(1);
+            emote.SetActive(false);
+        }
+        
+        public void AttackPlayer()
+        {
+            PlayerStats playerStats = target.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                stats.DealDamage(playerStats);
+            }
+        }
+        
+        private void GetReferences()
+        {
+            //get enemy stats
+            stats = GetComponent<EnemyStats>();
+        }
+    }
+}
